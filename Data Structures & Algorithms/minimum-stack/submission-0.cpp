efac7f628b8a1c554a471a7231a7db705fbc363d
Huangshouting27 <amberhuang0927@gmail.com>
2026-08-14 16:30:26 +0800

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,36 @@
+class MinStack {
+private:
+    stack<int> st;
+    stack<int> minst;
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        st.push(val);
+        if(minst.empty()) {
+            minst.push(val);
+        }
+        else{
+            if(val<=minst.top()) {
+                minst.push(val);
+            }
+        }
+    }
+    
+    void pop() {
+        if(st.top()==minst.top()) {
+            minst.pop();
+        }
+        st.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minst.top();
+    }
+};
